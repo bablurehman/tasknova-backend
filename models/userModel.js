b74918b98f5d2bc96f201ad3ada7b0ddeeea63b3
Bablu Rehman <bablureman111@gmail.com>
2026-02-27 19:05:40 +0530

    fix the mongoose schema

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, lowercase: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
     role: {
       type:String,
